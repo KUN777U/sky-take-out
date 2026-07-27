fce085ff8fa574b9199aee7c163afa94df5a6851
ZSHB <2052533505@qq.com>
2026-07-27 11:50:01 +0800

    苍穹外卖新增员工代码开发 （New employee code development）

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
  */
@@ -24,4 +26,19 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    /**
+     * 捕获SQL完整性约束异常
+     * @param ex   全局异常处理器
+     * @return
+     */
+    @ExceptionHandler
+    public Result execptionHandler(SQLIntegrityConstraintViolationException ex){
+        String message = ex.getMessage();
+        if (message.contains("Duplicate entry")){
+            String[] split = message.split("");
+            String s = split[2];
+            return Result.error("账号已存在");
+        }
+        return Result.error("未知错误");
+    }
 }
