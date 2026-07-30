755c23ce9e9e06c115013cf2e4f7fe72090503cb
ZSHB <2052533505@qq.com>
2026-07-30 13:13:41 +0800

    苍穹外卖修改菜品业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -40,6 +40,48 @@ public class DishServiceImpl implements DishService {
     private SetmaelDishMapper setmaelDishMapper;
 
     /**
+     * 根据id查询菜品及口味
+     * @param id
+     * @return
+     */
+    @Override
+    public DishVO getByIdWithFlavor(Long id) {
+        //根据id查询菜品数据
+        Dish dish = dishMapper.getById(id);
+        //根据菜品id查询口味数据
+        List<DishFlavor> dishFlavors = dishFlavorMapper.getByDishId(id);
+        //将查询到的数据封装到dishVO
+        DishVO dishVO = new DishVO();
+        BeanUtils.copyProperties(dish,dishVO);
+        dishVO.setFlavors(dishFlavors);
+        return dishVO;
+    }
+
+    /**
+     * 根据id修改菜品及口味
+     * @param dishDTO
+     */
+    @Override
+    public void updateWithFlavor(DishDTO dishDTO) {
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO,dish);
+        //修改菜品基本信息
+        dishMapper.updateDish(dish);
+
+        //删除原有的口味数据
+        dishFlavorMapper.deleteByDishId(dishDTO.getId());
+
+        //重新插入口味数据
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if(flavors != null && flavors.size() > 0){
+            flavors.forEach(dishFlavor -> {
+                dishFlavor.setDishId(dishDTO.getId());
+            });
+            dishFlavorMapper.insertBatch(flavors);
+        }
+    }
+
+    /**
      * 批量删除菜品
      * @param ids
      */
@@ -63,11 +105,20 @@ public class DishServiceImpl implements DishService {
             }
         }
         //删除菜品表中的数据
+/*
         for (Long id : ids) {
             dishMapper.deleteById(id);
             //删除菜品关联的口味数据
             dishFlavorMapper.deleteByDishId(id);
         }
+*/
+
+        //根据菜品id集合批量删除菜品数据
+        //sql:delete from dish where id in(?,?,?)
+        dishMapper.deleteByIds(ids);
+        //根据菜品id集合批量删除菜品口味数据
+        //sql:delete from dish_flavor where dish_id in(?,?,?)
+        dishFlavorMapper.deleteByDishIds(ids);
 
     }
 
