424bfc2d835007b8411cbd8c8bcca3f61f3be259
hh瑞 <17090190+hh-ruiii@user.noreply.gitee.com>
2026-08-15 21:07:18 +0800

    苍穹外卖运营数据报表代码开发

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 
@@ -90,5 +91,13 @@ public class ReportController {
         return Result.success(salesTop10ReportVO);
     }
 
+    /**
+     * 导出excel运营数据报表
+     */
+    @GetMapping("/export")
+    @ApiOperation("导出运营数据报表")
+    public void export(HttpServletResponse response) {
+        reportService.exportBusinessData(response);
 
+    }
 }
